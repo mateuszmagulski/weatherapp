159c751619d6b12e1c38d74972d7d35863fc0b73
mateuszmagulski <mateusz.magulski@gmail.com>
2020-03-31 15:24:08 +0200

    url cityname>pk, rest api

--- weather/urls.py
+++ weather/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
 from . import views
 
-urlpatterns = [
+urlpatterns = [ 
     path("", views.index, name="index"),
-    path("delete/<city_name>", views.delete_city, name="delete_city"),
+    path("delete/<int:pk>", views.delete_city, name="delete_city"),
+
+    path("api", views.api_overviwe, name="api_overview"),
+    path("api/list", views.api_list, name="api_list"),
+    path("api/details/<int:pk>", views.api_details, name="api_details"),
+    path("api/create", views.api_create, name="api_create"),
+    path("api/update/<int:pk>", views.api_update, name="api_update"),
+    path("api/delete/<int:pk>", views.api_delete, name="api_delete"),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,13 @@
 import requests
+
 from django.shortcuts import render, redirect
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import City
 from .forms import CityForm
+from .serializers import CitySerializer
 
 def index(request):
     url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=fca2224ce6af72370f76db5917959647"
@@ -44,6 +50,7 @@ def index(request):
         
         r = requests.get(url.format(city)).json()
         city_weather = {
+            "pk": city.pk,
             "city": city.name,
             "temperature": r["main"]["temp"],
             "description": r["weather"][0]["description"],
@@ -60,8 +67,106 @@ def index(request):
         }
     return render(request, 'weather/weather.html', context)
 
-def delete_city(request, city_name):
+def delete_city(request, pk):
     
-    City.objects.get(name=city_name).delete()
+    City.objects.get(pk=pk).delete()
 
     return redirect("index")
+
+@api_view(["GET"])
+def api_overviwe(request):
+    api_urls={
+        "List": "/api/list/",
+        "Detail": "/api/detail/<int:pk>",
+        "Create": "/api/create/",
+        "Update": "/api/update/<int:pk>",
+        "Delete": "/api/delete/<int:pk>"
+    }
+    return Response(api_urls)
+
+@api_view(["GET"])
+def api_list(request):
+    
+    cities = City.objects.all()
+    serializer = CitySerializer(cities, many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def api_details(request, pk):
+
+    city = City.objects.get(pk=pk)
+    serializer = CitySerializer(city, many=False)
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def api_create(request):
+
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=fca2224ce6af72370f76db5917959647"
+
+    err_msg = ""
+    message = ""
+    message_class = ""
+
+    serializer = CitySerializer(data=request.data)
+
+    if serializer.is_valid():
+        new_city = request.data["name"].title()
+        
+        existing_city_count = City.objects.filter(name=new_city).count()
+        
+        if existing_city_count == 0:
+            r = requests.get(url.format(new_city)).json()
+
+            if r["cod"] == 200:
+                City.objects.create(name=new_city)
+            else:
+                err_msg = "City does not exist in the world!"
+        else:
+            err_msg = "City already exists in the database!"
+    
+    if err_msg:
+        message = err_msg
+    else:
+        message = "City added successfully!"
+
+    return Response(message)
+
+@api_view(["POST"])
+def api_update(request, pk):
+
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=fca2224ce6af72370f76db5917959647"
+
+    err_msg = ""
+    message = ""
+    message_class = ""
+
+    city = City.objects.get(pk=pk)
+    serializer = CitySerializer(instance=city, data=request.data )
+
+    if serializer.is_valid():
+        new_city = request.data["name"].title()
+        
+        existing_city_count = City.objects.filter(name=new_city).count()
+        
+        if existing_city_count == 0:
+            r = requests.get(url.format(new_city)).json()
+
+            if r["cod"] == 200:
+                serializer.save()
+            else:
+                err_msg = "City does not exist in the world!"
+        else:
+            err_msg = "City already exists in the database!"
+    
+    if err_msg:
+        message = err_msg
+    else:
+        message = "City updated successfully!"
+
+    return Response(message)
+
+@api_view(["DELETE"])
+def api_delete(request, pk):
+    city = City.objects.get(pk=pk)
+    city.delete()
+    return Response("City deleted succesfully!")
